cc9be0d9e64a8f5ab5013d1bed484e89ce115559
cwastche <cwastche@student.s19.be>
2022-02-01 22:35:58 +0100

    empy ok

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: cwastche <cwastche@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/30 22:22:09 by cwastche          #+#    #+#             */
-/*   Updated: 2022/01/30 22:22:36 by cwastche         ###   ########.fr       */
+/*   Updated: 2022/02/01 22:32:09 by cwastche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,8 @@ int	main(int ac, char **av)
 	var.moves = 1;
 	line = map_read(av[1]);
 	var.map = ft_split(line, '\n');
+	if (!var.map)
+		return (0);
 	free(line);
 	map_check(&var);
 	var.mlx = mlx_init();
